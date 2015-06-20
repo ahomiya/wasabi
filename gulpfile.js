b02b21667f19e9bd7b650f317006569885cd79da
ahomiya.com (Mac mini) <me@ahomiya.com>
2015-06-20 16:41:08 +0700

    Add Gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+// Plugin dependencies
+var gulp            = require('gulp'),                        // Gulp
+    concat          = require('gulp-concat'),                 // Concatinate
+    uglify          = require('gulp-uglify')                  // Minify
+
+// -----------------------------------------------------------------------------
+// Configurations
+var packages        = './packages'                            // Packages
+
+// -----------------------------------------------------------------------------
+// Globs
+var js              = {
+    src             : './src',                               // JS - Sources
+    dist            : './dist'                               // JS - Distribution
+};
+
+// -----------------------------------------------------------------------------
+// Packages
+var packages        = {
+
+  // JavaScript libraries
+  js: {
+    utilities: [
+      js.src + '/**/*'
+    ]
+  }
+
+};
+
+// -----------------------------------------------------------------------------
+// Build tasks
+// Concatenating, minifying, and optimizing files
+
+// Concatenate
+gulp.task('js.build.concat', function() {
+  return gulp.src(packages.js.utilities)
+    .pipe(concat('wasabi.js'))
+    .pipe(gulp.dest(js.dist));
+});
+
+// Minified
+gulp.task('js.build.minify', function() {
+  return gulp.src(packages.js.utilities)
+    .pipe(concat('wasabi.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(js.dist));
+});
+
+// Custom
+gulp.task('js.build.custom', function() {
+  return gulp.src(packages.js.utilities)
+    .pipe(gulp.dest(js.dist));
+});
